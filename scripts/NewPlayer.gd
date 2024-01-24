1e049f9811d00aec4f44aa7a7d0c931a5782da5b
Alex Erdenberger <erdenberger.alexander@gmail.com>
2024-01-23 16:09:25 -0800

    character jumps

--- scripts/NewPlayer.gd
+++ scripts/NewPlayer.gd
@@ -5,7 +5,17 @@ extends CharacterBody3D
 var move_speed = 250
 var move_velocity: Vector3
 
-var gravity : float = 0.0
+var gravity:float = 0
+var jump_strength = 7
+
+var rotation_direction : float
+
+var previously_floored = false
+
+var jump_single = true
+var jump_double = true
+
+@onready var model = $character
 
 func _physics_process(delta):
 	handle_controls(delta)
@@ -17,6 +27,7 @@ func _physics_process(delta):
 	#Lerp - Returns the result of the linear interpolation between this vector and to by amount weight. 
 		#weight is on the range of 0.0 to 1.0, representing the amount of interpolation.
 	applied_velocity = velocity.lerp(move_velocity, delta * 10)
+	applied_velocity.y = -gravity
 	
 	velocity = applied_velocity
 	move_and_slide()
@@ -36,10 +47,22 @@ func handle_controls(delta):
 	input = input.rotated(Vector3.UP, view.rotation.y).normalized()
 	
 	move_velocity = input * move_speed * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		if(jump_single): 
+			jump()
+		
 	pass
+	
+func jump():
+	
+	gravity = -jump_strength
+	
+	jump_single = false
+	jump_double = true
 	
 func handle_gravity(delta):
 	gravity += 25 * delta
-	
 	if gravity > 0 and is_on_floor():
 		gravity = 0
+		jump_single = true
