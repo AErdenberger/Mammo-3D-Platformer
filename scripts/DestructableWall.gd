8d893a08b291edc57e620d2449b1ff2787ea6329
Alex Erdenberger <erdenberger.alexander@gmail.com>
2024-01-31 13:25:36 -0800

    boxes and bullets get destroyed

--- scripts/DestructableWall.gd
+++ scripts/DestructableWall.gd
@@ -1,5 +1,9 @@
 extends RigidBody3D
 
+@onready var MI:MeshInstance3D = $Model/Node/cuboid
+var material_full_health = preload("res://new_features/Mammo-3D-Platformer/models/Materials/Full_Health.tres")
+var material_damaged = preload("res://new_features/Mammo-3D-Platformer/models/Materials/Damaged.tres")
+var health = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +13,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	if body.owner.has_method("hitObject"):
+		if health > 1:
+			MI.set_surface_override_material(0, material_damaged)
+			health = health - 1
+		else:
+			queue_free()
+		body.queue_free()
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,3 +1,18 @@
 extends Node3D
 
 @onready var RB:RigidBody3D = $RigidBody3D
+
+var timer = Timer.new()
+
+func _ready():
+	timer.wait_time = 1
+	timer.timeout.connect(destroy)
+	timer.one_shot = true
+	add_child(timer)
+	timer.start()
+	
+func destroy():
+	queue_free()
+
+func hitObject():
+	pass
