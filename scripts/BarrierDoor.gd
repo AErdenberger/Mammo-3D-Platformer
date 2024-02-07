8ae93fbd068a3bd8f8bdb04815754898a741a809
Alex Erdenberger <erdenberger.alexander@gmail.com>
2024-02-06 16:05:29 -0800

    scripts created for button and door

---
+++ scripts/BarrierDoor.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@export var openDoor:bool
+@export var initScaleVector : Vector3
+@export var dirScaleVector : Vector3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
