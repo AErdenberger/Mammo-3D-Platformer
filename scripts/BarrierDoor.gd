834e5d428a58eb29f0a871c0dc9151cccfa10fbb
Alex Erdenberger <erdenberger.alexander@gmail.com>
2024-02-06 16:17:13 -0800

    refactor door "opening"

--- scripts/BarrierDoor.gd
+++ scripts/BarrierDoor.gd
@@ -3,21 +3,23 @@ extends Node3D
 @export var openDoor:bool
 @export var initScaleVector : Vector3
 @export var dirScaleVector : Vector3
+@export var objects : Array[Node3D]
 
 @onready var mesh = $RigidBody3D/cuboid
 @onready var collision = $RigidBody3D/CollisionShape3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initScaleVector = mesh.scale
+	initScaleVector = objects[0].scale
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(openDoor):
-		mesh.scale = dirScaleVector
-		collision.scale = dirScaleVector
+		for i in objects.size():
+			objects[i].scale = dirScaleVector
 	else:
-		mesh.scale = initScaleVector
-		collision.scale = initScaleVector
+		for i in objects.size():
+			objects[i].scale = initScaleVector
+
